90fe8f6b83a1dddd4062a9c47f33267133a4f342
AbdelkaderBensalem <61059360+AbdelkaderBensalem@users.noreply.github.com>
2022-12-06 17:09:31 +0100

    Add files via upload

---
+++ question-1/key_pair.tf
@@ -0,0 +1,4 @@
+resource "aws_key_pair" "mykey" {
+  key_name   = "Abdelkader-BENSALEM-key"
+  public_key = "AAAAB3NzaC1yc2EAAAADAQABAAABAQDY7B4dBzGv3b5jzFwfkImUd0e0CoCePushtlaT+Dunzkd95dfmdN/4VSSjNNt7SBGSQRXdi4p+XgpXbGNcEtoa4vRu1GbeXQdQ6X7+QzBTziuhRS5oVq/IEDzG/trlYBPKJnVbD46wlAh7RLYglscBQPXRyHo0UTAlmqF7yXOq5K+WJtpaQjl+VFLFrtDJFP7GHrTyyt5DOjGHlid9pRZIUpY1k0VtxR6HbnwXeh1VZnz2f9FZjnJoXNtwQdeBRZf7753Vpp7cyBJuMtbpnPQqKv5nC8p+KZDovB/pc2isrjWkn52lir5rgr6quQ5H0O9Ub0La1vtjkcpo0pixoOOV"
+}
